99bf605a927f4510a23fcdf45db7495c004c9143
Liam Flaherty <beatus@mwc-103215.dhcp.missouri.edu>
2017-09-22 09:11:13 -0500

    first add

--- Person UI/ViewController.swift
+++ Person UI/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func sliderAge(_ sender: Any) {
+        let slider = sender as! UISlider
+        let value = round(slider.value)
+        
+        displayOutlet.text = "\(value)"     }
+    
+    
 }
 
